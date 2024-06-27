05b0a68234574978039f1d3219351bf3909202e2
KarloLerga69 <karlolerga16@gmail.com>
2024-06-27 02:03:01 +0200

    thx

---
+++ src/main/java/com/example/projektoopkarlol/ProjektOopKarloLApplication.java
@@ -0,0 +1,13 @@
+package com.example.projektoopkarlol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjektOopKarloLApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjektOopKarloLApplication.class, args);
+	}
+
+}
